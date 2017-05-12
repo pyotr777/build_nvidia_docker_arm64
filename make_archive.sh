c511e7826ffd8a64802e9f1248cc5c6f383c9a2f
Peter Bryzgalov <peterbryz@yahoo.com>
2017-05-12 18:44:41 +0900

    Add host cuda files to container using tar archive.

---
+++ make_archive.sh
@@ -0,0 +1,8 @@
+tar czvf libsharecuda.tgz /usr/share/doc/cuda-cudart-dev-8-0/ \
+		/usr/lib/pkgconfig/ \
+		/usr/lib/openmpi/ \
+		/usr/share/doc/cuda-misc-headers-8-0/ \
+		/usr/local/cuda-8.0/include \
+		/usr/local/cuda-8.0/lib64 \
+		/usr/local/cuda-8.0/targets \
+		/usr/local/cuda-8.0/bin
